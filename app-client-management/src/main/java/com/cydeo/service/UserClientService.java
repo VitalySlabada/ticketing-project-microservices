828cff73c14e947cf08b8c4c70482dc3a753e111
VitalySlabada <vitaly.slabada@gmail.com>
2022-07-27 19:55:02 -0500

    part 3 done

--- app-client-management/src/main/java/com/cydeo/service/UserClientService.java
+++ app-client-management/src/main/java/com/cydeo/service/UserClientService.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(value = "user-service",url = "http://localhost:9090")
+@FeignClient("user-service")
 public interface UserClientService {
 
     @GetMapping("/api/v1/user/{username}")
