6648c55b9171fa7cf5ec3635990e768fa0376493
VitalySlabada <vitaly.slabada@gmail.com>
2022-07-30 17:47:49 -0500

    part9 -security

--- gateway-service/src/main/java/com/cydeo/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/cydeo/GatewayServiceApplication.java
@@ -19,25 +19,25 @@ public class GatewayServiceApplication {
 
     }
 
-//    @Bean
-//    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
-//        return builder.routes()
-//                .route(p -> p
-//                        .path("/cydeo/user/**")
-//                        .filters(f -> f.rewritePath("/cydeo/user/(?<segment>.*)","/${segment}")
-//                                .addResponseHeader("X-Response-Time",new Date().toString()))
-//                        .uri("lb://user-service"))
-//                .route(p -> p
-//                        .path("/cydeo/project/**")
-//                        .filters(f -> f.rewritePath("/cydeo/project/(?<segment>.*)","/${segment}")
-//                                .addResponseHeader("X-Response-Time",new Date().toString()))
-//                        .uri("lb://project-service"))
-//                .route(p -> p
-//                        .path("/cydeo/task/**")
-//                        .filters(f -> f.rewritePath("/cydeo/task/(?<segment>.*)","/${segment}")
-//                                .addResponseHeader("X-Response-Time",new Date().toString()))
-//                        .uri("lb://task-service"))
-//                .build();
-//    }
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p
+                        .path("/cydeo/user/**")
+                        .filters(f -> f.rewritePath("/cydeo/user/(?<segment>.*)","/${segment}")
+                                .addResponseHeader("X-Response-Time",new Date().toString()))
+                        .uri("lb://user-service"))
+                .route(p -> p
+                        .path("/cydeo/project/**")
+                        .filters(f -> f.rewritePath("/cydeo/project/(?<segment>.*)","/${segment}")
+                                .addResponseHeader("X-Response-Time",new Date().toString()))
+                        .uri("lb://project-service"))
+                .route(p -> p
+                        .path("/cydeo/task/**")
+                        .filters(f -> f.rewritePath("/cydeo/task/(?<segment>.*)","/${segment}")
+                                .addResponseHeader("X-Response-Time",new Date().toString()))
+                        .uri("lb://task-service"))
+                .build();
+    }
 
 }
